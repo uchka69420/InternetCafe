a961660523901820bd753d279d97caa1afc0613e
tamjid dorjderem <sukhee278@gmail.com>
2022-01-14 16:36:30 +0800

    psda

--- InternetCafe/Form2.Designer.cs
+++ InternetCafe/Form2.Designer.cs
@@ -34,11 +34,11 @@ namespace InternetCafe
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(356, 151);
+            this.button1.Location = new System.Drawing.Point(206, 143);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(427, 141);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "tamjid";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form2
