688a9678f64b076a608af63980cd8cb3975178fb
tamjid dorjderem <sukhee278@gmail.com>
2022-01-14 16:30:03 +0800

    za

--- InternetCafe/Form2.Designer.cs
+++ InternetCafe/Form2.Designer.cs
@@ -34,7 +34,7 @@ namespace InternetCafe
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(232, 101);
+            this.button1.Location = new System.Drawing.Point(326, 137);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
